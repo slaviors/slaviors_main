894dc999e03b096a6073fec901e1148202e65259
mamatqurtifa <mqurtifawijaya007@gmail.com>
2025-12-09 04:41:46 +0700

    feat(home): add WhyChooseUsSection to highlight key advantages

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import { CtaSection } from "@/components/home/cta-section"
 import { HeroSection } from "@/components/home/hero-section"
 import { ServicesSection } from "@/components/home/service-section"
+import { WhyChooseUsSection } from "@/components/home/why-choose-us-section"
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <ServicesSection />
+      <WhyChooseUsSection />
       <CtaSection />
     </>
   )
